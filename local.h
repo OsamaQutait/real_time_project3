57c562324acb2f5770b3f9f3f5b5917195591ade
Osama Qutait <oaqutait@gmail.com>
2023-01-15 21:01:46 +0200

    type A

--- local.h
+++ local.h
@@ -1,7 +1,3 @@
-//
-// Created by osama on 1/13/23.
-//
-
 #ifndef PROJECT3_LOCAL_H
 #define PROJECT3_LOCAL_H
 #include <iostream>
@@ -9,21 +5,36 @@ using namespace std;
 #include <unistd.h>
 #include <pthread.h>
 #include <vector>
-
+#include <string.h>
+#include <queue>
+#include <unordered_set>
 #define RED     "\033[31m"      /* Red */
 #define GREEN   "\033[32m"      /* Green */
 #define BLUE    "\033[34m"      /* Blue */
 #define YELLOW  "\033[33m"      /* Yellow */
 #define BLACK   "\033[30m"      /* Black */
 #define MAGENTA "\033[35m"      /* Magenta */
+int minA, maxA, minB, maxB, minC, maxC, number_of_line ,
+number_of_employee_of_typeA, number_of_employee_of_typeB, number_of_employee_of_typeC;
+int order[4];
+unordered_set<int> unordered_set1 = {1, 2, 3, 4};
 typedef struct employees
 {
     int line;
     int worker; // number of step
 } employees;
+typedef struct chocolate
+{
+    unsigned int chocolate_id;
+    char type;
+    int finished_steps;
+    pthread_mutex_t chocolate_mutex;
+} chocolate;
+vector<chocolate> list_of_chocolate;
+queue<chocolate> queue1[9][9];
 /*
  * mutex look
  */
-pthread_mutex_t typeA_l1_mutex, typeA_l2_mutex, typeA_l3_mutex;
-pthread_cond_t typeA_l1_cond, typeA_l2_cond, typeA_l3_cond;
+pthread_mutex_t typeA_l1_mutex[4], typeA_l2_mutex, typeA_l3_mutex;
+pthread_cond_t typeA_l1_cond[9], typeA_l2_cond, typeA_l3_cond;
 #endif //PROJECT3_LOCAL_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 #include "local.h"
+
+void read_file() {
+    char tmp[30];
+    FILE *arguments;
+    arguments = fopen("inputData.txt", "r");
+    if (arguments == NULL) {
+        perror("CANT OPEN FILE");
+        exit(1);
+    }
+    fscanf(arguments, "%s %s %d %d\n", tmp, tmp, &minA, &maxA);
+    fscanf(arguments, "%s %s %d %d\n", tmp, tmp, &minB, &maxB);
+    fscanf(arguments, "%s %s %d %d\n", tmp, tmp, &minC, &maxC);
+    fscanf(arguments, "%s %d\n", tmp, &number_of_line);
+    fscanf(arguments, "%s %d\n", tmp, &number_of_employee_of_typeA);
+    fscanf(arguments, "%s %d\n", tmp, &number_of_employee_of_typeB);
+    fscanf(arguments, "%s %d\n", tmp, &number_of_employee_of_typeC);
+}
+
 int generate_waiting_time(int lower, int upper) {
     int s, numb;
     FILE *f;
@@ -10,48 +28,184 @@ int generate_waiting_time(int lower, int upper) {
     return num;
 }
 
-void *line1(void *arg){
-    employees c = *(employees *)arg;
-    pthread_mutex_lock(&typeA_l1_mutex);
-    cout << GREEN << c.line << endl;
-    cout << GREEN << c.worker << endl;
-    cout << BLUE << pthread_self() << endl;
-    fflush(stdout);
-    pthread_mutex_unlock(&typeA_l1_mutex);
+void *line1(void *arg) {
+    sleep(1);
+    employees c = *(employees *) arg;
+    if (c.worker == 0) {
+        int z = 1;
+        while (1) {
+            chocolate coco;
+            coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+            pthread_mutex_lock(&coco.chocolate_mutex);
+            coco.chocolate_id = z;
+            coco.type = 'A';
+            coco.finished_steps = 1;
+            sleep(generate_waiting_time(minA, maxA));
+            queue1[c.line][c.worker].push(coco);
+            cout << GREEN << "employee " << c.worker << " produce item with step "
+                 << queue1[c.line][c.worker].back().finished_steps << " id " << z << endl;
+            fflush(stdout);
+            z += 1;
+            pthread_mutex_unlock(&coco.chocolate_mutex);
+        }
+    }
+    if (c.worker == 1) {
+        while (1) {
+            if (!queue1[c.line][c.worker - 1].empty()) {
+                chocolate coco = queue1[c.line][c.worker - 1].front();
+                queue1[c.line][c.worker - 1].pop();
+                coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                pthread_mutex_lock(&coco.chocolate_mutex);
+                sleep(generate_waiting_time(minA, maxA));
+                coco.finished_steps++;
+                cout << RED << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                     << coco.chocolate_id << endl;
+                fflush(stdout);
+                queue1[c.line][c.worker].push(coco);
+                pthread_mutex_unlock(&coco.chocolate_mutex);
+            }
+        }
+    }
+
+    if (c.worker == 2) {
+        while (1) {
+            if (!queue1[c.line][c.worker - 1].empty()) {
+                chocolate coco = queue1[c.line][c.worker - 1].front();
+                queue1[c.line][c.worker - 1].pop();
+                coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                pthread_mutex_lock(&coco.chocolate_mutex);
+                sleep(generate_waiting_time(minA, maxA));
+                coco.finished_steps++;
+                cout << BLUE << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                     << coco.chocolate_id << endl;
+                fflush(stdout);
+                queue1[c.line][c.worker].push(coco);
+                pthread_mutex_unlock(&coco.chocolate_mutex);
+            }
+        }
+    }
+
+    if (c.worker == 3) {
+        while (1) {
+            if (!queue1[c.line][c.worker - 1].empty()) {
+                chocolate coco = queue1[c.line][c.worker - 1].front();
+                queue1[c.line][c.worker - 1].pop();
+                coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                pthread_mutex_lock(&coco.chocolate_mutex);
+                sleep(generate_waiting_time(minA, maxA));
+                coco.finished_steps++;
+                cout << YELLOW << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                     << coco.chocolate_id << endl;
+                fflush(stdout);
+                queue1[c.line][c.worker].push(coco);
+                pthread_mutex_unlock(&coco.chocolate_mutex);
+            }
+        }
+    }
+
+    if (c.worker == 4 || c.worker == 5 || c.worker == 6 || c.worker == 7){
+        pthread_mutex_lock(&typeA_l1_mutex[c.line]);
+        order[c.line]++;
+        pthread_mutex_unlock(&typeA_l1_mutex[c.line]);
+
+        if (order[c.line] == 1){
+            while (1){
+                if (!queue1[c.line][3].empty()) {
+                    chocolate coco = queue1[c.line][3].front();
+                    queue1[c.line][3].pop();
+                    coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                    pthread_mutex_lock(&coco.chocolate_mutex);
+                    sleep(generate_waiting_time(minA, maxA));
+                    coco.finished_steps++;
+                    cout << GREEN << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                         << coco.chocolate_id << endl;
+                    fflush(stdout);
+                    queue1[c.line][4].push(coco);
+                    pthread_mutex_unlock(&coco.chocolate_mutex);
+                }
+            }
+        }
+        if (order[c.line] == 2){
+            while (1){
+                if (!queue1[c.line][4].empty()) {
+                    chocolate coco = queue1[c.line][3].front();
+                    queue1[c.line][4].pop();
+                    coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                    pthread_mutex_lock(&coco.chocolate_mutex);
+                    sleep(generate_waiting_time(minA, maxA));
+                    coco.finished_steps++;
+                    cout << RED << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                         << coco.chocolate_id << endl;
+                    fflush(stdout);
+                    queue1[c.line][5].push(coco);
+                    pthread_mutex_unlock(&coco.chocolate_mutex);
+                }
+            }
+        }
+        if (order[c.line] == 3){
+            while (1){
+                if (!queue1[c.line][5].empty()) {
+                    chocolate coco = queue1[c.line][3].front();
+                    queue1[c.line][5].pop();
+                    coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                    pthread_mutex_lock(&coco.chocolate_mutex);
+                    sleep(generate_waiting_time(minA, maxA));
+                    coco.finished_steps++;
+                    cout << RED << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                         << coco.chocolate_id << endl;
+                    fflush(stdout);
+                    queue1[c.line][6].push(coco);
+                    pthread_mutex_unlock(&coco.chocolate_mutex);
+                }
+            }
+        }
+        if (order[c.line] == 4){
+            while (1){
+                if (!queue1[c.line][6].empty()) {
+                    chocolate coco = queue1[c.line][3].front();
+                    queue1[c.line][6].pop();
+                    coco.chocolate_mutex = PTHREAD_MUTEX_INITIALIZER;
+                    pthread_mutex_lock(&coco.chocolate_mutex);
+                    sleep(generate_waiting_time(minA, maxA));
+                    coco.finished_steps++;
+                    coco.type = 'A';
+                    cout << BLUE << "employee " << c.worker << " produce item step" << coco.finished_steps << " id is "
+                         << coco.chocolate_id << endl;
+                    fflush(stdout);
+                    queue1[c.line][7].push(coco);
+                    pthread_mutex_unlock(&coco.chocolate_mutex);
+                }
+            }
+        }
+    }
+
     free(arg);
     return NULL;
 }
+
 int main() {
-    /*
-     * line 1, 2, 3 type A
-     * line 4, 5 type B
-     * line 5, 6 type C
-     *
-     * type A has 8 employees
-     * type B has 6 employees
-     * type C has 5 employees
-     */
-    pthread_mutex_init(&typeA_l1_mutex, NULL);
-    pthread_cond_init(&typeA_l1_cond, NULL);
+    read_file();
+    memset(order, 0, sizeof(order));
     pthread_t thread[8];
+    for (int i = 0; i < 4; ++i) {
+        typeA_l1_mutex[i] = PTHREAD_MUTEX_INITIALIZER;
+    }
     for (int i = 0; i < 8; ++i) {
         employees x;
-        x.line = 1;
+        x.line = 0;
         x.worker = i; //at ane step
-        employees *emp = (employees *)malloc(sizeof(employees));
+        employees *emp = (employees *) malloc(sizeof(employees));
         *emp = x;
-        if(pthread_create(thread+i, NULL, &line1, emp) != 0){
+        if (pthread_create(thread + i, NULL, &line1, emp) != 0) {
             perror("pthread_create");
             exit(-1);
         }
     }
     for (int i = 0; i < 8; ++i) {
-        if (pthread_join(thread[i], NULL) != 0){
+        if (pthread_join(thread[i], NULL) != 0) {
             perror("pthread_create");
             exit(-1);
         }
     }
-    pthread_mutex_destroy(&typeA_l1_mutex);
-    pthread_cond_destroy(&typeA_l1_cond);
     return 0;
 }
